121546931075996c952548e4903f936b8d783e67
AnastasiaCoder <mstitelnica1999@gmail.com>
2022-02-24 20:08:29 +0700

    added config

---
+++ src/test/java/gmail/anastasiacoder/config/ApiConfig.java
@@ -0,0 +1,14 @@
+package gmail.anastasiacoder.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/api.properties"
+})
+public interface ApiConfig extends Config {
+
+    String apiUrl();
+
+}
---
+++ src/test/java/gmail/anastasiacoder/config/CredentialsConfig.java
@@ -0,0 +1,15 @@
+package gmail.anastasiacoder.config;
+
+import org.aeonbits.owner.Config;
+
+@Config.LoadPolicy(Config.LoadType.MERGE)
+@Config.Sources({
+        "system:properties",
+        "classpath:config/credentials.properties"
+})
+public interface CredentialsConfig extends Config {
+
+    String userName();
+
+    String password();
+}
